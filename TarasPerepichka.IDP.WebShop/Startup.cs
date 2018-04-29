24c7c5dc4af57e3ca8c7058d7ef09965807e2bde
Daniel Shumov <lewshumov@gmail.com>
2018-04-29 10:37:52 +0300

    Add project files.

---
+++ TarasPerepichka.IDP.WebShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TarasPerepichka.IDP.WebShop.Startup))]
+namespace TarasPerepichka.IDP.WebShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

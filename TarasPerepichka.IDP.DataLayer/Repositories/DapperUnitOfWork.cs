4dbf1c95e218a1d2219273b0247c660c59a9ce05
Taras Perepichka <tperepichka@natmedtrans.com>
2019-03-01 18:56:00 +0200

    commit idp version

---
+++ TarasPerepichka.IDP.DataLayer/Repositories/DapperUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TarasPerepichka.IDP.DataLayer.DataContext;
+using TarasPerepichka.IDP.DataLayer.Entitties;
+using TarasPerepichka.IDP.DataLayer.Interfaces;
+
+namespace TarasPerepichka.IDP.DataLayer.Repositories
+{
+    public class DapperUnitOfWork : IUnitOfWork
+    {
+        private ArticleRepository articleRepository;
+        private OrderRepository orderRepository;
+        public string dbConnection;
+
+        public DapperUnitOfWork(string connectionString)
+        {
+            dbConnection = connectionString;
+        }
+
+        public IRepository<ArticleEntity> Articles
+        {
+            get { return articleRepository ?? (articleRepository = new ArticleRepository(dbConnection)); }
+        }
+
+        public IRepository<OrderEntity> Orders
+        {
+            get { return orderRepository ?? (orderRepository = new OrderRepository(dbConnection)); }
+        }
+    }
+}
